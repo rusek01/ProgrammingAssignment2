8b8e02bf5405fe5a88952f673abe57093cafad78
rusek01 <a.rusinowski@gmail.com>
2015-07-26 23:18:49 +0200

    assigment done

--- cachematrix.R
+++ cachematrix.R
@@ -2,8 +2,23 @@
 ## functions do
 
 ## Write a short comment describing this function
+## function below was desingned along the lines of makeVector example function
+## i only changet names  from mean to inv, however becouse programers need to be lazy i didnt even change m variable :)
+## so now this function has 4 lets call them methods 1 - to set matrix (and wipe cache), 2 - to return that matrix
+## 3 - to set cache with inverted matrix, and 4 - to get inverted matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) m <<- inv
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 
 }
 
@@ -11,32 +26,18 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
-
-
-makeVector <- function(x = numeric()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setmean <- function(mean) m <<- mean
-  getmean <- function() m
-  list(set = set, get = get,
-       setmean = setmean,
-       getmean = getmean)
-}
-
-cachemean <- function(x, ...) {
-  m <- x$getmean()
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  ## becouse of how makeCacheMatrix works, if !is.null(m) is true we
+  ## know that original matrix didn't change, becouse if we used $set method from makeCacheMatrix
+  ## and thats only way to change original matrix, we set value of m to null
   if(!is.null(m)) {
     message("getting cached data")
     return(m)
   }
-  data <- x$get()
-  m <- mean(data, ...)
-  x$setmean(m)
+  d <- x$get() ## code to get matrix
+  m <- solve(d)  ## code to invert matrix
+  x$setinv(m)
   m
 }
+
